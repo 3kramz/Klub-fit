6e9716061ec316875d434e9366b012141556607c
Ekramz <smahmud1098@gmail.com>
2022-04-18 20:15:26 +0600

    README.md

--- src/Components/Pages/About/About.js
+++ src/Components/Pages/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div className="container text-center">
-             <img  className='img ' src='https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1650277533~exp=1650278133~hmac=cb1aefa189e6f93dce7d0d90592638f40cc411c5feb3b3ded11137f2858500ad' alt="" srcset="" />
+             <img  className='img ' src='https://img.freepik.com/free-vector/mysterious-mafia-man-smoking-cigarette_52683-34828.jpg?w=740&t=st=1650277533~exp=1650278133~hmac=cb1aefa189e6f93dce7d0d90592638f40cc411c5feb3b3ded11137f2858500ad' alt="" />
              <h2 className="text-info">Sultan Mahmud Ekram</h2>
              <p className="fs-3">My name is Ekram, 22, still a student, looking for a part time job I can do while still at school, ideally working remotely. I’m skilled with javascript and React, and I am trying to learn other programming languages, step by step. Have two brothers, both of them study at a college, so it is quite funny that I, as a boy, decided for software engineering. I try to get the most out of my time at the college, and have some edge once I graduate, which is the primary reason why I seek a part time job with you.</p>
         </div>
